78cfbff29259a22420e835c3d8529f3fec944137
Amanda Johnson <amanda.j86@gmail.com>
2024-06-23 17:00:29 -0700

    update to checkbox being on correct side. Delete button added.

--- script.js
+++ script.js
@@ -11,6 +11,50 @@ const assignmentList = document.getElementById('assignment-list');
 
 //DONE-TODO create a new item assignent in the unordered list
 
+//takes in the text to add a new assignment to the assignment list
+function addNewAssignment(assignment) { 
+  const listItem = document.createElement('li');
+  const assignmentText = document.createElement('span')
+  
+  assignmentText.textContent = assignment;
+
+
+  //TODO Create a way to show the assignment is done
+  //create a checkbox checkbox is intentianal 
+  const checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+
+  //create delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'delete'
+
+  //append the checkbox before the assignment text add delete button
+  listItem.appendChild(checkBox)
+  listItem.appendChild(assignmentText);
+  listItem.appendChild(deleteButton)
+
+  //append list item to assignment list
+  assignmentList.appendChild(listItem);
+  console.log(assignmentList);
+
+    //event listener delete button
+  deleteButton.addEventListener('click', function() {
+    assignmentList.removeChild(listItem);
+    console.log(assignmentList);
+  });
+
+  //need event listener for assignment completion
+  checkBox.addEventListener('change', function() {
+  if (this.checked) {
+    assignmentText.style.textDecoration = 'line-through';
+  } else {
+    assignmentText.style.textDecoration = 'none'
+  }
+
+
+});
+}
+
 //event listener for the add assignment button
 assignmentForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -23,33 +67,6 @@ assignmentForm.addEventListener('submit', function(event) {
   userInput.value = ''; // will clear the input field after assignment is added
   addNewAssignment(newAssignment)
 });
-
-//takes in the text to add a new assignment to the assignment list
-function addNewAssignment(assignment) { 
-  const listItem = document.createElement('li');
-  const assignmentText = document.createElement('span')
-  
-  assignmentText.textContent = assignment;
-  listItem.appendChild(assignmentText);
-  assignmentList.appendChild(listItem);
-  console.log(assignmentList);
-
-  //TODO Create a way to show the assignment is done
-  const checkBox = document.createElement('input');
-  checkBox.setAttribute('type', 'checkbox');
-  listItem.appendChild(checkBox)
-
-  //need event listener for assignment completion
-checkBox.addEventListener('change', function() {
-  if (this.checked) {
-    assignmentText.style.textDecoration = 'line-through';
-  } else {
-    assignmentText.style.textDecoration = 'none'
-  }
-
-});
-}
-
 
 
 
